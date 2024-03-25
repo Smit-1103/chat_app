b86068dd9fc5f3cda3e528b79d0930d7fc819793
smit patel <smitpatel7032@gmail.com>
2024-03-26 01:48:04 +0530

    chore: bug fix

--- lib/widgets/new_message.dart
+++ lib/widgets/new_message.dart
@@ -1,5 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class NewMessage extends StatefulWidget {
   const NewMessage({super.key});
@@ -9,44 +10,64 @@ class NewMessage extends StatefulWidget {
 }
 
 class _NewMessageState extends State<NewMessage> {
-  var _messageController = TextEditingController();
-  FocusNode _messageFocusNode = FocusNode();
+  final _messageController = TextEditingController();
+  // FocusNode _messageFocusNode = FocusNode();
 
   @override
   void dispose() {
     _messageController.dispose();
-    _messageFocusNode.dispose();
+    // _messageFocusNode.dispose();
     super.dispose();
   }
 
-  void _openKeyboard() {
-     // Focus the text field
-    _messageFocusNode.requestFocus();
+  // void _openKeyboard() {
+  //   // Focus the text field
+  //   _messageFocusNode.requestFocus();
 
-    // Open the native emoji keyboard
-    Future.delayed(Duration(milliseconds: 100), () {
-      if (Theme.of(context).platform == TargetPlatform.iOS) {
-        // For iOS devices, use the system method to show the keyboard
-        SystemChannels.textInput.invokeMethod('TextInput.show');
-      } else if (Theme.of(context).platform == TargetPlatform.android) {
-        // For Android devices, use the system method to show the emoji keyboard
-        SystemChannels.textInput.invokeMethod('TextInput.showInputMethodPicker');
-      }
-    });
-  }
+  //   // Open the native emoji keyboard
+  //   Future.delayed(const Duration(milliseconds: 100), () {
+  //     if (Theme.of(context).platform == TargetPlatform.iOS) {
+  //       // For iOS devices, use the system method to show the keyboard
+  //       SystemChannels.textInput.invokeMethod('TextInput.show');
+  //     } else if (Theme.of(context).platform == TargetPlatform.android) {
+  //       // For Android devices, use the system method to show the emoji keyboard
+  //       SystemChannels.textInput
+  //           .invokeMethod('TextInput.showInputMethodPicker');
+  //     }
+  //   });
+  // }
 
-  void _submitMessage() {
+  void _submitMessage() async {
     final enteredMessage = _messageController.text;
 
     if (enteredMessage.trim().isEmpty) {
       return;
     }
 
-    // Send to firebase
-    // ...
+    // this is used to close the keyboard after sending the message
+    FocusScope.of(context).unfocus();
 
     // Clear the textfield after sending the message
     _messageController.clear();
+    // to get the name of currently logged in user we have to take name from the firebase
+    final user = FirebaseAuth.instance.currentUser;
+    final userData = await FirebaseFirestore.instance
+        .collection('user')
+        .doc(user?.uid)
+        .get();
+
+    
+    print('WE ARE AFTER THEINSTANCE');
+
+    // Send to firebase
+    FirebaseFirestore.instance.collection('chat').add({
+      'text': enteredMessage,
+      'createdAT': Timestamp.now(),
+      'userId': user?.uid,
+      'username': userData.data()!['username'],
+      'userImage': userData.data()!['image_url'],
+    });
+    print('WE ARE AFTER THE FIREBASEFIRESTORE INSTANCE');
   }
 
   @override
@@ -65,11 +86,12 @@ class _NewMessageState extends State<NewMessage> {
             child: Row(
               children: [
                 IconButton(
-                    icon: const Icon(
-                      Icons.emoji_emotions_outlined,
-                    ),
-                    onPressed: _openKeyboard,
+                  icon: const Icon(
+                    Icons.emoji_emotions_outlined,
                   ),
+                  onPressed: () {},
+                  // onPressed: _openKeyboard,
+                ),
                 const SizedBox(
                   width: 5,
                 ),
@@ -83,7 +105,7 @@ class _NewMessageState extends State<NewMessage> {
                       padding: const EdgeInsets.symmetric(horizontal: 19),
                       child: TextField(
                         controller: _messageController,
-                        focusNode: _messageFocusNode,
+                        // focusNode: _messageFocusNode,
                         textCapitalization: TextCapitalization.sentences,
                         autocorrect: true,
                         enableSuggestions: true,
